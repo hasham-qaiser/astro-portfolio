103055f425be5fed14835b386aabc4991f0514ec
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-09-18 13:13:54 -0700

    added recent contributions edited about me

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client-image" />
+
+interface ImportMetaEnv {
+	readonly SPOTIFY_REFRESH_TOKEN: string;
+	readonly SPOTIFY_CLIENT_ID: string;
+	readonly SPOTIFY_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
